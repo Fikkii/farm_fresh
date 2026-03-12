21d79f3931476ae9890bbc858b18ffe5599f1398
fikkii <ajalafikayo@gmail.com>
2026-03-12 02:37:34 +0100

    Added a dummy homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Link } from 'react-router-dom'
+import { Button } from '@heroui/react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div className='grid place-items-center h-screen'>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 className='text-[32px] font-bold'>Page Under Construction</h1>
+        check out the
+        <div className='flex gap-3'>
+          <Link className='w-full' to="/auth/login"><Button className='w-full' color="success">Login</Button></Link>
+          <Link to="/auth/verify"><Button color="success">Verify</Button></Link>
+          <Link to="/auth/signup"><Button color="success">Signup</Button></Link>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
--- src/pages/VerifyPage.jsx
+++ src/pages/VerifyPage.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 
 export default function VerifyPage() {
   return (
-    <div className="bg-[#FAFAFA] p-[24px] w-[65%] rounded-[20px] flex flex-col gap-[16px]">
+    <div className="bg-[#FAFAFA] p-[24px] w-full md:w-[70%] lg:w-[65%] rounded-[20px] flex flex-col gap-[16px]">
       <h1 className="font-bold text-[24px] text-center">Verify your account</h1>
       <p className="text-sm text-center text-[#757575]">We've sent a 4-digit code to your email. Please enter it
 below to complete your registration.</p>
