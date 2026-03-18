07d2e186ba1c656cf9eecc89f15ec83d2896b530
fikkii <ajalafikayo@gmail.com>
2026-03-18 13:16:16 +0100

    Connected the Landing page to the database

---
+++ src/components/cards/FarmCardGroup.jsx
@@ -0,0 +1,47 @@
+import {Card, CardBody, CardFooter, Image} from "@heroui/react";
+import { useNavigate } from "react-router-dom";
+
+export default function CardComponent({data}) {
+  const navigate = useNavigate()
+  return (
+    <div className="gap-[24px] mt-[42px] flex flex-col md:grid grid-cols-2 sm:grid-cols-4">
+      {data.map((item, index) => (
+        /* eslint-disable no-console */
+        <Card key={index} isPressable shadow="sm" onPress={() =>  navigate(`/farms/${item.$id}`)}>
+          <CardBody className="overflow-visible p-0">
+            <Image
+              alt={item.farmName}
+              className="w-full object-cover h-[192px]"
+              radius="lg"
+              shadow="sm"
+              src={item.img}
+              width="100%"
+            />
+          </CardBody>
+          <CardFooter className="text-small flex-col items-start">
+            <div>
+              <b className="text-[18px]">{item.farmName}</b>
+            </div>
+            <div className="flex justify-between w-full gap-4">
+            <p className="flex items-center text-[#EAB308]">
+              {item.rating}
+              <svg xmlns="http://www.w3.org/2000/svg" fill="#EAB308" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+              </svg>
+
+            </p>
+            <p className="flex items-center text-gray-500">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+              </svg>
+              {item.location}
+            </p>
+        </div>
+        </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
+}
+
